6bb30b4879e1c16f97787602e0bc54b973737ae4
masahiro-sakane <hiro182799@gmail.com>
2026-05-19 13:13:18 +0900

    fix: Google ProviderにclientId/clientSecretを明示指定

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -2,7 +2,12 @@ import type { NextAuthConfig } from 'next-auth'
 import Google from 'next-auth/providers/google'
 
 export const authConfig: NextAuthConfig = {
-  providers: [Google],
+  providers: [
+    Google({
+      clientId: process.env.AUTH_GOOGLE_ID,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET,
+    }),
+  ],
   trustHost: true,
   session: { strategy: 'jwt' },
   callbacks: {
